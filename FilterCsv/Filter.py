d3271597d8444f2d6804be46beb0ce6bd926b06f
SaidaHumbert <saidita.hf@gmail.com>
2022-08-27 01:27:55 +0200

    Test logs in Python

--- FilterCsv/Filter.py
+++ FilterCsv/Filter.py
@@ -1,5 +1,9 @@
 import csv
 import sys
+import logging
+from os import mkdir
+import errno
+from datetime import datetime
 
 aggCSV = "./resources/agg.csv"
 mvtCSV = "./resources/mvt.csv"
@@ -18,15 +22,17 @@ class Filter:
         s1, s2 = sys.argv[1], sys.argv[2]
         if s1 is None:
             print("Please, add the first parameter 'entity': ")
+            logging.error("ERROR: Parameter 'entity' not detected. The aplication need this parameter.")
         self.filterByValue(CSVData, s1, "entity")
         if s2 is not None:
             self.filterByValue(CSVData, s2, "currency")
 
     def filterByValue(self, CSVData, value, nameColumn):
-        isEquals = True
+        isEquals = False
         count = 0
         nameColumn = nameColumn.lower()
         if nameColumn not in CSVData[0]:
+            logging.error("ERROR: Not found value in CSV file")
             raise Exception(f"{nameColumn} not found in csv file")
         positionColumn = CSVData[0].index(nameColumn)
         for i in range(len(CSVData)):
@@ -39,6 +45,7 @@ class Filter:
             else:
                 if value not in CSVData[i][positionColumn]:
                     # print(CSVData[i])  # -> resultado del filtro
+                    # status not expected
                     count += 1
 
         print(f"{value} is {count} times")
@@ -63,7 +70,18 @@ class Filter:
 
 
 if __name__ == "__main__":
-    test = Filter()
-    test.loadProgram()
+    try:
+        mkdir('logs')
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    pathLogFile = "./logs/" + datetime.today().strftime('%Y-%m-%d_%H_%M_%S') + ".log"
+    logging.basicConfig(filename=pathLogFile, encoding='utf-8', level=logging.DEBUG)
+    # logging.debug('This message should go to the log file')
+    # logging.info('So should this')
+    # logging.warning('And this, too')
+    # logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+    filter = Filter()
+    filter.loadProgram()
     # test.readFileCsv(aggCSV)
     # test.askParametter()
